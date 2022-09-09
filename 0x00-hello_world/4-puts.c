502ed30f1d40cb1f2df9a6c35cd92a55918dcf4a
Derrick Rogers MALUMBA <malumbaderrick@gmail.com>
2022-09-08 17:17:49 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0
- *     */
+
+ /**
+  * main - Entry point
+  *
+  * Return: 0 in this case
+  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
